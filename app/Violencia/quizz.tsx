4810bad5f2fe99749097097ea0687da2e34f85c1
LesiRock <isel.rebolledo@gmail.com>
2025-10-17 13:06:52 -0600

    Create quizz.tsx

---
+++ app/Violencia/quizz.tsx
@@ -0,0 +1,16 @@
+import { Colors } from '@/constants/Colors';
+import { globalStyles } from '@/styles/globalStyles';
+import React from 'react';
+import { Image, Text, View } from 'react-native';
+
+
+export default function quizz(){
+
+    return(
+        <View>
+            <Image 
+            style={{height: 370, width: "100%", resizeMode:"contain"}}
+            source=/>
+        </View>
+    )
+}
